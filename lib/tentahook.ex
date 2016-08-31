2e5f22ff1c7f038e88d2363a35b15309f92b6aaa
Hubert Jarosz <marqin.pl@gmail.com>
2016-08-31 18:15:54 +0200

    make start_link/1 accept map of options

--- lib/tentahook.ex
+++ lib/tentahook.ex
@@ -7,8 +7,8 @@ defmodule Tentahook do
     end
   end
 
-  def start_link(handler) do
-    child = Plug.Adapters.Cowboy.child_spec(:http, Tentahook.DI, [handler: handler], [])
+  def start_link(tentahook_opts = %{handler: handler}) do
+    child = Plug.Adapters.Cowboy.child_spec(:http, Tentahook.DI, tentahook_opts, [])
     opts = [strategy: :one_for_one]
     Supervisor.start_link [child], opts
   end
